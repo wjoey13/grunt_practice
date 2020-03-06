681e729bd2f3828a144685463ae586e740c4fe2c
joseph walter <wjoey13@gmail.com>
2020-03-05 18:18:05 -0700

    fixed syntax error

--- logger.js
+++ logger.js
@@ -1,11 +1,23 @@
-function logger() {
-  if (count === 10) {
-    console.log('Now that\'s a garden!');
-  }
-  if (count > 50 && count < 75) {
-    console.log('Oh man, ' + count + ' tomatoes, what more can you do?');
-  }
-  if (count === 100) {
-    console.log(count + ' tomatoes is too much, you can\'t handle that much acid.');
-  }
+// function logger() {
+//   if (count === 10) {
+//     console.log('Now that\'s a garden!');
+//   }
+//   if (count > 50 && count < 75) {
+//     console.log('Oh man, ' + count + ' tomatoes, what more can you do?');
+//   }
+//   if (count === 100) {
+//     console.log(count + ' tomatoes is too much, you can\'t handle that much acid.');
+//   }
+// }
+
+let logger = (count) => {
+    if (count == 10) {
+        console.log('Now That\'s a garden!');
+    }
+    if (count > 50 && count < 75) {
+        console.log(`Oh man, ${count} tomatoes, what more can you do?`);
+    }
+    if (count == 100) {
+        console.log(`${count} tomatoes is too much, you can\'t handle that much acid.`);
+    }
 }
